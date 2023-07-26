30f738e61eb6cce4e204f16ca468615900b4bc5d
Qunthunnan <bredtv@gmail.com>
2023-07-26 16:09:15 +0300

    26.07.2023 release

---
+++ dist/script/phone-mask.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+    $("input[name=telephone]").mask("+380 (99)-999-99-99");
+
+});
---
+++ dist/script/validator.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+    // console.log("TEST1");
+    function validateForm (form){
+        // console.log("TEST2");
+        let validation = $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                telephone: {
+                    required: true
+                },
+                email: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    minlength: "Мінімальна кількість букв для імені: 2",
+                    required: "Будь ласка, напишіть своє ім'я"
+                },
+                telephone: "Будь ласка, напишіть свій телефон",
+                email: {
+                    required: "Будь ласка, напишіть свою електронну скриньку",
+                    email: "Перевірте, чи правильно написана адреса, не забудьте @"
+                }
+            },
+            errorClass: "client-form__error"
+        });
+
+        // console.log("TEST3");
+        return validation.form();
+    };
+
+
+    $("form").submit(function (e) {
+        // console.log("TESt4");
+        // console.log(e);
+        // console.log(this);
+        e.preventDefault();
+        if(validateForm(this) == true) {
+            $('.client-form__loading').fadeIn();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("form").trigger("reset");
+
+            $('.client-form__loading').fadeOut();
+            $(".overlay .modal").fadeOut('slow');
+            // console.log('preFadeIn');
+            $(".overlay, #buy-done").fadeIn('slow');
+            // console.log('afterFadeIn');
+        })
+        }
+
+        return false;
+    });
+});
